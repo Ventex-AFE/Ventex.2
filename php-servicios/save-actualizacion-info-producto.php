c970a9377e41e7d69b4b33915c56f67ab54f6a08
Kazu N <emmanuel.gonzalez1438@alumnos.udg.mx>
2024-04-29 20:09:25 -0600

    create te files, in progress

---
+++ php-servicios/save-actualizacion-info-producto.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+// Credenciales de acceso a la base de datos
+$hostname = ''; //Url de la direccion dela base de datos 
+$username = ''; //Usuario que se uso para esta conexion y la verifcacion 
+$password = ''; //Password del usuario 
+$database = 'ventex'; //nombre de la db
+
+$Conexion = mysqli_connect($hostname, $username, $password, $database);
+if (mysqli_connect_error()) {
+    exit('Fallo en la conexión de MySQL: ' . mysqli_connect_error());
+}
+$Nombre_produc=$_POST['Nombre_Prod'];
+
+?>
