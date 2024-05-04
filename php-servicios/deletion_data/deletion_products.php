b3935117109569b5d1293ca308374a8be709b395
Kazu000001 <emmanuel.gonzalez1438@alumnos.udg.mx>
2024-05-04 14:52:27 -0600

    deletion products - login funcional

---
+++ php-servicios/deletion_data/deletion_products.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('../Conexion_db/conexion_usser_delete.php');
+
+    //$eliminar=$_POST['eliminar'];
+    $eliminar = 6; // Suponiendo que este es un valor válido para eliminar un registro de la base de datos
+    $sql = "DELETE FROM productos WHERE ID_Producto = $eliminar";
+    $delete = mysqli_query($Conexion_usser_delete, $sql);
+
+    if ($delete) {
+        header('Location: ../../Frames/pantalla-perfil.php');
+        exit(); // Agregamos exit() para asegurarnos de que el script se detenga después de la redirección
+    } else {
+        echo 'Hubo un error al eliminar el registro.';
+    }
+?>
