ecb11aa1fab1fcc9b1a376b5f6151bb30fac64ed
Edd-Spr <145872901+Edd-Spr@users.noreply.github.com>
2024-05-07 23:25:09 -0600

    Actualización pantalla pedidos

--- Scripts/Script-pedidos.js
+++ Scripts/Script-pedidos.js
@@ -6,6 +6,9 @@ const modalForm = document.querySelector('.modalFormMainContainer');
 const formOverlay = document.querySelector('.formOverlay');
 const addOrderButton = document.querySelector('.addOrder');
 const input = document.querySelectorAll('.input');
+const optionsButton = document.querySelectorAll('.checkButton');
+const optionsList = document.querySelectorAll('.pointsOptions');
+const invisibleOverlay = document.querySelector('.invisibleOverlay');
 
 const openModal = () =>{ // This function opens the modal
     modal.classList.remove('hidden');
@@ -26,8 +29,22 @@ const closeModalForm = () =>{ //This function close the form modal
         input[i].value = '';
     }
 }
+const closeOptionsList = () =>{
+    optionsList.forEach((list) => {
+            list.classList.add('hidden');
+            invisibleOverlay.classList.add('hidden');
+    });
+}
 
 calcular.addEventListener('click', openModal);
 overlay.addEventListener('click', closeModal);
 addOrderButton.addEventListener('click', openModalForm);
 cancelButton.addEventListener('click', closeModalForm);
+
+optionsButton.forEach((but, index) => {
+    but.addEventListener('click', () => {
+        optionsList[index].classList.remove('hidden');
+        invisibleOverlay.classList.remove('hidden');
+    });
+});
+invisibleOverlay.addEventListener('click', closeOptionsList);
