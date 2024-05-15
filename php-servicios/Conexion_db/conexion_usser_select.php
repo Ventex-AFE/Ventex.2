c648d72237a7144541ce20cd60177aea926eb98e
Edd-Spr <145872901+Edd-Spr@users.noreply.github.com>
2024-05-15 13:40:08 -0600

    Re ajustes de diseño pantalla de busqueda y perfil vista cliente

--- php-servicios/Conexion_db/conexion_usser_select.php
+++ php-servicios/Conexion_db/conexion_usser_select.php
@@ -1,10 +1,10 @@
 <?php
 // Credenciales de acceso a la base de datos
 $hostname = '127.0.0.1'; //Url de la direccion dela base de datos 
-// $username = 'Usser_consult';
-$username = 'root'; //Usuario que se uso para esta conexion y la verifcacion 
-//$password = 'Dw0&Q=]o95F]Wlj5y/TMvt:=UX'; //Password del usuario 
-$password = ''; //Password del usuario 
+$username = 'Usser_consult';
+//$username = 'root'; //Usuario que se uso para esta conexion y la verifcacion 
+$password = 'Dw0&Q=]o95F]Wlj5y/TMvt:=UX'; //Password del usuario 
+//$password = ''; //Password del usuario 
 $database = 'ventexafe'; //nombre de la db
 
 // Conexión a la base de datos
