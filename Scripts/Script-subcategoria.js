15474bfcb4d75aa18c47d8e6b93340132c84d787
Edd-Spr <145872901+Edd-Spr@users.noreply.github.com>
2024-05-19 16:25:30 -0600

    Cambios Subcategorias

--- Scripts/Script-subcategoria.js
+++ Scripts/Script-subcategoria.js
@@ -1,35 +1,61 @@
 const products = document.querySelectorAll('.productContainer');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-let cont = 0;
+const prevButton = document.querySelectorAll('.prev');
+const nextButton = document.querySelectorAll('.next');
+const carrusel = document.querySelectorAll('.productsCarrusel');
 
-const prev = () =>{
+const conter = [];
+
+
+const prev = (ind) =>{
     
-    if(cont == 0){
-        cont = Math.ceil(products.length / 5) - 1;
-    }
-    else{
-        cont--;
-    }
-    scroll();
-}
-const next = () =>{
 
-    if(cont >= Math.ceil(products.length / 5) - 1){
-        cont = 0;
+    if((Math.ceil(products.length / 5) - 1) > 0){
+        console.log('hay mas de 5 productos');
+        if(conter[ind] = 0){
+            console.log('bich');
+            conter[ind] = Math.ceil(products.length / 5) - 1;
+        }
+        else{
+            conter[ind]--;
+            console.log('bich 2');
+        }
+        scroll(ind);
     }
-    else{
-        cont++;
+}
+const next = (ind) =>{
+
+    console.log(conter);
+    if((Math.ceil(products.length / 5) - 1) > 0){
+        console.log('hay mas de 5 productos next');
+        if(conter[ind] >= Math.ceil(products.length / 5) - 1){
+            conter[ind] = 0;
+        }
+        else{
+            conter[ind]++;
+        }
+        
+        scroll(ind);
     }
-    
-    scroll();
 }
 
-const scroll = () =>{
-    products.forEach(item =>{
-        item.style.transform = `translateX(-${cont * 80}vw)`;
+const scroll = (ind) =>{
+    carrusel.forEach((item, index) =>{
+        if(ind == index){
+            item.style.transform = `translateX(-${conter[ind] * 80}vw)`;
+        }
     });
 }
 
-prevButton.addEventListener('click', prev);
-nextButton.addEventListener('click', next);
+carrusel.forEach((car, index) =>{
+
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+
+carrusel.forEach((prevBut, index) => {
+    conter.push(0);
+    prevButton[index].addEventListener('click', () => prev(index));
+    nextButton[index].addEventListener('click', () => next(index));
+});
+
+});
