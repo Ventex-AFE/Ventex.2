2a556eb57539dbe5c3b7b35634a6942ba83e0db3
Edd-Spr <145872901+Edd-Spr@users.noreply.github.com>
2024-05-21 15:43:22 -0600

    Cambios

--- Frames/Pantalla-Subcategoria.php
+++ Frames/Pantalla-Subcategoria.php
@@ -1,9 +1,8 @@
 <?php
-$us = 'Thomas Rogrigez';
 require_once('../php-servicios/Conexion_db/conexion_usser_select.php');
 $cats = mysqli_query($Conexion_usser_select, "SELECT DISTINCT Nombre_Cat FROM categoria;");
 
-$mywher = /*isset($_GET['categoria']) ? $_GET['categoria'] : null */'Ropa';
+$mywher = isset($_GET['categoria']) ? $_GET['categoria'] : null /*'Ropa'*/;
 $busc = null;
 
 if ($mywher != null) {
