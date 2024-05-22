8082db7e101f30a0c0af854bdb567d9ab4d634c3
ferconh <143868890+ferconh@users.noreply.github.com>
2024-05-22 01:26:39 -0600

    Ajustar estilos

--- Frames/Pantalla-Edit-Info-Personal.php
+++ Frames/Pantalla-Edit-Info-Personal.php
@@ -49,8 +49,7 @@ mysqli_stmt_close($stmt);
     <link rel="stylesheet" href="../Styles/Styles-Edit-Info-Personal.css">
     <link rel="stylesheet" href="../Componentes/header.css">
     <link rel="stylesheet" href="../Componentes/footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <!-- Barra de navegación -->
@@ -74,74 +73,89 @@ mysqli_stmt_close($stmt);
     </header>
     <!-- Contenido principal -->
     <main>
-        <section>
-            <!-- Sección lateral 1 -->
-            <section id="side1">
-                <h1 id="Descrip-AgregarP">Actualiza</h1>
-                <h3 id="Descrip-AgregarP2">Información Personal</h3>
-            </section>
+<!-- ------parte de decoración -->
+        <section class="decor">
+            <div class="circle1"></div>
+            <div class="circle2"></div>
+            <div class="circle3"></div>
+        </section>
+
+        <section class="form">
             <!-- Sección lateral 2 (formulario para actualizar redes sociales) -->
-            <section id="side2">
-                <section id="imagen-usser-container">
-                    <img src="../Icons/perfilPic.png" class="imagen-usser" id="imgPreview" >
-                </section>
-                <form action="..\php-servicios\save_data\save-actualizacion-datos-personales.php" method="post" enctype="multipart/form-data">
-                    <br>
-                    <!-- Inputs para actualizar datos de redes sociales -->
-                    <div class="inputbox" style="height: 6vh;">
+            <h1 class="form-title">Actualizar Información Personal</h1>
+            <form action="..\php-servicios\save_data\save-actualizacion-datos-personales.php" method="post" enctype="multipart/form-data">
+                <!-- Inputs para actualizar datos de redes sociales -->
+                <div id="up-photo">
+                    <label for="Imagen">Añadir Imagen</label>
                     <input type="file" name="Imagen" class="inp" accept="image/*"  onchange="previewImage(event, '#imgPreview')">
-                    <script>
-                        function previewImage(event, querySelector){
-                        const input = event.target;
-                        $imgPreview = document.querySelector(querySelector);
+                <script>
+                    function previewImage(event, querySelector){
+                    const input = event.target;
+                    $imgPreview = document.querySelector(querySelector);
 
-                        if(!input.files.length) return
+                    if(!input.files.length) return
 
-                        file = input.files[0];
+                    file = input.files[0];
 
-                        objectURL = URL.createObjectURL(file);
+                    objectURL = URL.createObjectURL(file);
 
-                        $imgPreview.src = objectURL;
-                        }
+                    $imgPreview.src = objectURL;
+                    }
                     </script>
                     </div>
                         <input type="hidden" name="imagenanterior" value="<?php echo $Imagen ?>">
 
-                    <div class="inputbox" style="height: 6vh;">
+                    <div class="inputbox">
+                        <label for="nombre">Nombre(s):</label>
                         <input type="text" name="nombre" class="inp" placeholder="" value="<?php echo $Nombre_Us ?>" required><br>
-                        <span class="text_input">Nombre(s):</span>
                     </div>
 
-                    <div class="inputbox" style="height: 6vh;">
+                    <div class="inputbox">
+                        <label for="correo">Correo</label>
                         <input type="email" name="correo" class="inp" placeholder="" value="<?php echo $Correo ?>" required><br>
-                        <span class="text_input">Email</span>
                     </div>
 
-                    <div class="inputbox" style="height: 6vh;">
+                    <div class="inputbox">
+                        <label for="fecha">Fecha de Nacimiento</label>
                         <input type="date" name="fecha" class="inp" placeholder="" value="<?php echo $Fecha_Nac ?>" required><br>
-                        <span class="text_input">Fecha Nacimiento</span>
                     </div>
 
                     <div class="inputbox" style="height: 6vh;">
+                        <label for="telefono">Teléfono</label>
                         <input type="number" name="telefono" class="inp" placeholder="" value="<?php echo $telefono ?>" required><br>
-                        <span class="text_input">Telefono</span>
                     </div>
                     <!-- Botón para enviar el formulario -->
                     <div id="button-div">
-                        <button type="submit" id="button-sumit">Actualizar</button>
+                        <br><br><button type="submit" class="submit" id="button-sumit">Actualizar</button>
                     </div>
                 </form>
-            </section>
         </section>
+
+        <section class="cont-img">
+            <img src="../Icons/perfilPic.png" class="imagen-usser" id="imgPreview" >
+        </section>
+
     </main>
     <!-- Pie de página -->
     <footer>
-        <section class="name-year"><h1>2023-Ventex</h1></section>
-        <section class="logo-ventex"><h1>Ventex</h1></section>
-        <section class="socialmedia-ventex">
-            <a href=""><i class="fa-brands fa-facebook"></i></a>
-            <a href=""><i class="fa-brands fa-square-x-twitter"></i></a>
-            <a href=""><i class="fa-brands fa-tiktok"></i></a>
+        <section class="con">
+            <section class="name-year">
+                <h1>2023-Ventex</h1>
+            </section>
+            <section class="logo-ventex">
+                <h1>Ventex</h1>
+            </section>
+            <section class="socialmedia-ventex">
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
+                <a href=""><i class="fa-brands fa-square-x-twitter"></i></a>
+                <a href=""><i class="fa-brands fa-tiktok"></i></a>
+            </section>
+        </section>
+        <section class="aviso">
+            <span>Ventex no pide a través de SMS o de las redes sociales datos bancarios, tarjetas de crédito, clave NIP,
+                contraseñas o datos sensibles de cualquier tipo. 
+                <br>Si necesitas aclarar cualquier duda, puedes contactar con el Call Center en 800 225 5748.
+            </span>
         </section>
     </footer>
 </body>
